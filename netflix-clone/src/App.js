8bf1c88bfcbce206a612b627e19e9131ef8055e9
sanjay-singh-patel <sanjupatel11112000@gmail.com>
2023-02-26 07:22:56 +0000

    accessing api data

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,9 +1,15 @@
+import React from 'react';
 import './App.css';
+import Row from './Rows';
+import requests from './requests';
 
 function App() {
   return (
     <div className="App">
       <h1> Netflix</h1>
+      <Row title="Netflix Originals" fetchURL={requests.fetchNetflixOriginals}/>
+      <Row title="Trending" fetchURL={requests.fetchTrending}/>
+
     </div>
   );
 }
