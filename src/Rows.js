bb10bb1d21d31141699a525024feeb285ff0a030
Sanjay Singh Patel <sanjupatel11112000@gmail.com>
2023-03-06 02:12:34 +0530

    Started with setting up the movies database api

--- src/Rows.js
+++ src/Rows.js
@@ -1,11 +1,14 @@
 import React, { useState , useEffect} from 'react';
 import axios from "./axios";
+import './response.json'
+
+const base_url = 'http://image.tmdb.org/t/p/original/'
 
 function Row({title,fetchUrl}) {
     const [movies,setMovies]=useState([]);
     useEffect(() => {
         async function fetchData(){
-            const request = await axios.get(fetchUrl);
+            const request = this.props.responsef;
             setMovies(request.data.results);
             return request;
         }
@@ -18,7 +21,7 @@ function Row({title,fetchUrl}) {
             <h2>{title}</h2>
             <div className='row_poster'>
                 {movies.map(movie =>(
-                    <img src={movie.poster_path} alt={movie.name}>
+                    <img src={'${base_url}${movie.poster_path}'} alt={movie.name}>
                     </img>
                 ))}
             </div>
